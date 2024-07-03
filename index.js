15626de03f21adc136d320a31e06cc3ee2664631
ryanowino <owinoryan@gmail.com>
2024-07-04 00:17:40 +0300

    complete labb

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function receivesAFunction(callback) {
   
   function returnsANamedFunction() {
     return function namedFunction() {
-      // Some implementation
+      
     };
   }
   
   function returnsAnAnonymousFunction() {
     return function() {
-      // Some implementation
+      
     };
   }
   
